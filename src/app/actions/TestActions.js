71c4b2b477d9e2ba9baa03b8bd5577792751f028
pdbq21 <pdbq2122@gmail.com>
2017-01-27 17:50:07 +0200

    commit

--- src/app/actions/TestActions.js
+++ src/app/actions/TestActions.js
@@ -15,3 +15,37 @@ export function testActions() {
         });
     }
 }
+
+/*export function testActions(searchType, title) {
+    return {
+        type: types.LOAD_SEARCH_LAYOUT,
+        searchType,
+        title
+    };
+}*/
+
+// Example actions
+let nextTodoId = 0;
+export const addTodo = (text) => {
+    return {
+        type: 'ADD_TODO',
+        id: nextTodoId++,
+        text
+    }
+};
+
+export const setVisibilityFilter = (filter) => {
+    return {
+        type: 'SET_VISIBILITY_FILTER',
+        filter
+    }
+};
+
+export const toggleTodo = (id) => {
+    return {
+        type: 'TOGGLE_TODO',
+        id
+    }
+};
+
+//Todo: in State 'id' each element is - example => .0.0.1.2
--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -1,10 +1,13 @@
 /**
  * Created by ruslan on 27.01.17.
  */
+// lib
 import { combineReducers } from 'redux';
 
+// Reducers
 import test from './test';
 
+// Combine Reducers
 export default combineReducers({
-    test
+    test: test
 })
