b23f5f09e156e8e0cb93580e9552dce2be4df716
pdbq21 <pdbq2122@gmail.com>
2017-02-23 16:55:13 +0200

    pull

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 //import App from './app/App'
-import Builder from './app/containers/BuilderContainer';
+//import Builder from './app/containers/BuilderContainer';
 import configureStore from './app/store/configureStore';
 // import styles
 //app 3.0
-<<<<<<< HEAD
 //import BuilderComponent from './app_v3.0/components/BuilderComponent'
-=======
+
 import BuilderComponent from './app_v3.0/components/BuilderComponent'
 import './app_v3.0/styles/bootstrap/bootstrap-reboot.css';
 import './app_v3.0/styles/bootstrap/bootstrap-grid.css';
@@ -19,19 +18,18 @@ import './app_v3.0/styles/theme.css';
 import './app_v3.0/styles/module.css';
 import './app_v3.0/styles/layout.css';
 import './app_v3.0/styles/state.css';
->>>>>>> 4c37eb9103da95bf373474eecd17845c3c242b07
 import './app_v3.0/styles/index.css';
 // styles web kit
 
-import './styles/material.css'
-import './styles/pb.css'
-import './styles/main.css'
+//import './styles/material.css'
+//import './styles/pb.css'
+//import './styles/main.css'
 
 const store = configureStore();
 
 ReactDOM.render(
     <Provider store={store}>
-            <Builder />
+            <BuilderComponent />
     </Provider>,
   document.getElementById('root-app-pb')
 );
