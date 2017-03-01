f156a49273ad3ed0b64ca09c15b29e1ea4ff6121
Yurii Rabeshko <george.rabeshko@gmail.com>
2017-03-01 22:10:34 +0200

    Create toggle button

--- src/app_v3.0/components/Toolbar/ContentMenu/ContentPanels/StylesPanelComponent.js
+++ src/app_v3.0/components/Toolbar/ContentMenu/ContentPanels/StylesPanelComponent.js
@@ -14,6 +14,22 @@ export default function StylesPanelComponent() {
             <h3 className="pb-menu-caption">
                 <i className="fa fa-paint-brush pb-menu-caption-icon"/>Edit
             </h3>
+
+            <div className="pb-panel">
+                <h4 className="pb-panel-caption">Favorites</h4>
+
+                <div className="pb-panel-content">
+                    <div className="pb-option">
+                        <label htmlFor="jt-1" className="pb-option-name">Toggle button</label>
+                        <div className="pb-option-value">
+                            <div className="pb-joystick pb-toggle-joystick">
+                                <input type="checkbox" id="jt-1" className="pb-toggle-joystick-value" />
+                                <label htmlFor="jt-1" className="pb-toggle-joystick-btn"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
