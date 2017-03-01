4395076325070e9e9df78a109b01dd8d4ac1cb85
pdbq21 <pdbq2122@gmail.com>
2017-03-01 10:13:21 +0200

    done: toggle navigation items

--- src/app_v3.0/containers/Toolbar/MainMenuContainer.js
+++ src/app_v3.0/containers/Toolbar/MainMenuContainer.js
@@ -13,51 +13,64 @@ import MainMenuComponent from '../../components/Toolbar/MainMenu/MainMenuCompone
 import * as ToolbarActions from '../../actions/ToolbarActions'
 
 class MainMenuContainer extends Component {
-    constructor(props) {
-        super(props);
+		constructor(props) {
+				super(props);
 
-        this.handleToggleContentMenu = this.handleToggleContentMenu.bind(this);
-        this.handelNavigationItems = this.handelNavigationItems.bind(this);
-    }
+				this.handleToggleContentMenu = this.handleToggleContentMenu.bind(this);
+				this.handelNavigationItems = this.handelNavigationItems.bind(this);
+		}
 
-    componentDidMount() {
-        // empty
-    }
+		componentDidMount() {
+				// empty
+		}
 
-    handleToggleContentMenu() {
-        const {ActionToggleContentMenu} = this.props.mapDispactchToolbar;
-        ActionToggleContentMenu();
-    }
+		handleToggleContentMenu() {
+				const {ActionToggleContentMenu} = this.props.mapDispactchToolbar;
+				ActionToggleContentMenu();
+		}
 
-    handelNavigationItems(event) {
-        const {ActionSelectMenuItem} = this.props.mapDispactchToolbar;
-        //console.log(event.target.getAttribute('title'));
-        // event.target.getAttribute('title') => Rows/Elements/Edit/Template
-        ActionSelectMenuItem(event.target.getAttribute('title'));
-    }
+		handelNavigationItems(event) {
+				const {ActionSelectMenuItem} = this.props.mapDispactchToolbar;
+				//console.log(event.target.getAttribute('title'));
+				// event.target.getAttribute('title') => Rows/Elements/Edit/Template
+				ActionSelectMenuItem(event.target.getAttribute('title'));
+		}
 
-    render() {
+		render() {
+				const {menuItems, activeMenuItem} = this.props.mapStateToolbar;
+				return (
+						<MainMenuComponent
 
-        return (
-            <MainMenuComponent
-                handleToggleContentMenu={this.handleToggleContentMenu}
-                handelNavigationItems={event => this.handelNavigationItems(event)}
-            />
-        );
-    }
+								handleToggleContentMenu={this.handleToggleContentMenu}
+								handelNavigationItems={event => this.handelNavigationItems(event)}
+						>
+								{menuItems.map((item, index) => (
+										<li
+												key={`key-menu-${item.name}`}
+												className={(activeMenuItem === item.name) ? 'pb-active-menu-item' : ''}
+												title={item.name}
+										>
+												<i className={`fa ${item.classIcon}`}
+													 style={{'pointerEvents': 'none'}}
+												/>
+										</li>
+								))}
+						</MainMenuComponent>
+				);
+		}
 }
 
 
 function mapStateToProps(state) {
-    return {
-        mapStateToolbar: state.ToolbarReducer
-    }
+		return {
+				mapStateToolbar: state.ToolbarReducer
+		}
 }
 
 function mapDispatchToProps(dispatch) {
-    return {
-        mapDispactchToolbar: bindActionCreators(ToolbarActions, dispatch)
-    }
+		return {
+				mapDispactchToolbar: bindActionCreators(ToolbarActions, dispatch)
+		}
 }
 
 //WorkAreaContainer = DragDropContext(HTML5Backend)(WorkAreaContainer);
