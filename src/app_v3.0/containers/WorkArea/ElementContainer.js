73bf77bde61a15d67db70e3148ee3eb2dd1ceaf1
pdbq21 <pdbq2122@gmail.com>
2017-03-01 11:22:01 +0200

    done: drop elements

--- src/app_v3.0/containers/WorkArea/ElementContainer.js
+++ src/app_v3.0/containers/WorkArea/ElementContainer.js
@@ -5,30 +5,80 @@
 import React, {Component} from 'react'
 import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
-
+import {DropTarget} from 'react-dnd';
 // components
 import ElementComponent from '../../components/WorkArea/ElementComponent'
 //containers
 // actions
 import * as WorkAreaActions from '../../actions/WorkAreaActions'
 
+function DropAreaElement(props) {
+		const {canDrop, isOver, connectDropTarget} = props;
+		const isActive = canDrop && isOver;
+		let styles;
+
+		if (isActive) {
+				styles = {
+						'visibility': 'visible',
+						'background': 'linear-gradient(0deg, #14af35 0%, rgba(255, 255, 255, 0) 100%)'
+				};
+		} else if (canDrop) {
+				styles = {'visibility': 'visible'};
+		}
+		return connectDropTarget(
+        <div
+            className="pb-drop-zone"
+            style={styles}
+        />
+		);
+}
+
+const DropAreaElementTarget = DropTarget('DROP_ELEMENT', {
+		drop(props, monitor) {
+
+				props.onDrop(monitor.getItem());
+				return {};
+		}
+}, (connect, monitor) => {
+		return {
+				connectDropTarget: connect.dropTarget(),
+				isOver: monitor.isOver(),
+				canDrop: monitor.canDrop()
+		};
+})(DropAreaElement);
+
 // Application
 class ElementContainer extends Component {
-		/*constructor(props) {
+		constructor(props) {
 				super(props);
-		}*/
+				this.handleDropElement = this.handleDropElement.bind(this);
+		}
 
+		handleDropElement(parentId, id, item){
+		    // parentId => id Col; id => drop id Element; item => {elementType: 'Text'/'Image' ..}
+//console.log('drop element', parentId, id, item);
+				const {ActionCreateNode, ActionAddNode, ActionElementType} = this.props.mapDispactchWorkArea;
+				const childrenIdElement = ActionCreateNode(parentId).nodeId;
+				// create Element
+				ActionAddNode(parentId, childrenIdElement);
+				// add element type => Text/Image/Button...
+				ActionElementType(childrenIdElement, item.elementType);
+    }
 
 		render() {
 				const {elementType} = this.props.mapStateElement;
-				//const {id} = this.props;
+				const {id, parentId} = this.props;
 
 				return (
-            <ElementComponent
-                type={elementType}
-            >
-            </ElementComponent>
-
+				    <div>
+                <ElementComponent
+                    type={elementType}
+                >
+                </ElementComponent>
+                <DropAreaElementTarget
+                    onDrop={item => this.handleDropElement(parentId, id, item)}
+                />
+            </div>
 				);
 		}
 }
