cacc055e159c6b75cf70c8d8d2a514be34b20df0
Yurii Rabeshko <george.rabeshko@gmail.com>
2017-03-06 14:54:23 +0200

    Config TinyMCE

--- src/app_v3.0/components/WorkArea/ElementComponent.js
+++ src/app_v3.0/components/WorkArea/ElementComponent.js
@@ -7,111 +7,112 @@ import TinyMCE from 'react-tinymce';
 
 //import component
 export default function ElementComponent(props) {
-		const {type, handleClickContextMenu, handleEditorChange, styles} = props;
-		let element;
-		if (typeof styles.background === "undefined") {
-				switch (type) {
+    const {type, handleClickContextMenu, handleEditorChange, styles} = props;
+    let element;
+    if (typeof styles.background === "undefined") {
+        switch (type) {
 
-						case 'image':
-								element = (<div className="pb-element pb-image-element pb-default-element">
-										<i className="pb-image-element-icon fa fa-image"/>
-										<span className="pb-image-element-caption">
+            case 'image':
+                element = (<div className="pb-element pb-image-element pb-default-element">
+                    <i className="pb-image-element-icon fa fa-image"/>
+                    <span className="pb-image-element-caption">
                                             Drop your image here OR:
                                         </span>
-										<button className="pb-image-element-button btn btn-primary">
-												Select an image
-												{/* e.preventDefault() -> click on input below */}
-										</button>
-										<input type="file" className="pb-image-element-input"/>
-								</div>);
-								break;
-						case 'icon':
-								element = (<div className="pb-element pb-icon-element">
-										<i className="fa fa-grav"/>
-								</div>);
-								break;
-						default:
-								break;
-				}
-		} else {
-				const {r, g, b, a} = styles.background.backgroundColor;
-				switch (type) {
-						case 'text':
-								element = (
-										<div
-												className="pb-element pb-text-element"
-												style={{'backgroundColor': `rgba(${r},${g},${b},${a})`}}
-										>
-												<TinyMCE
-														content="<p>This is the initial content of the editor</p>"
-														config={{
-																inline: true,
-																linkchecker_api_key: 'idds1ja21nirk6ca3rxkpxi0mirk22w2zyvzz1f3y15dzelg',
-																plugins: 'link image code',
-																toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code'
-														}}
-														onChange={handleEditorChange}
-												/>
-										</div>);
-								break;
-						case 'button':
-								element = (
-										<div
-												className="pb-element pb-button-element"
-												style={{'backgroundColor': `rgba(${r},${g},${b},${a})`}}
-										>
-												<a href="#" className="btn btn-primary">
-														Sample button text
-														{/* e.preventDefault() */}
-												</a>
-										</div>);
-								break;
-						case 'heading':
-								element = (
-										<div
-												className="pb-element pb-heading-element"
-												style={{'backgroundColor': `rgba(${r},${g},${b},${a})`}}
-										>
-												<TinyMCE
-														content="<h2>This is simple heading</h2>"
-														config={{
-																inline: true,
-																linkchecker_api_key: 'idds1ja21nirk6ca3rxkpxi0mirk22w2zyvzz1f3y15dzelg',
-																plugins: 'link image code',
-																toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code'
-														}}
-														onChange={handleEditorChange}
-												/>
-										</div>);
-								break;
-						case 'link':
-								element = (
-										<div
-												className="pb-element pb-link-element"
-												style={{'backgroundColor': `rgba(${r},${g},${b},${a})`}}
-										>
-												<a href="#">
-														Sample link
-														{/* e.preventDefault() */}
-												</a>
-										</div>);
-								break;
+                    <button className="pb-image-element-button btn btn-primary">
+                        Select an image
+                        {/* e.preventDefault() -> click on input below */}
+                    </button>
+                    <input type="file" className="pb-image-element-input"/>
+                </div>);
+                break;
+            case 'icon':
+                element = (<div className="pb-element pb-icon-element">
+                    <i className="fa fa-grav"/>
+                </div>);
+                break;
+            default:
+                break;
+        }
+    } else {
+        const {r, g, b, a} = styles.background.backgroundColor;
+        switch (type) {
+            case 'text':
+                element = (
+                    <div
+                        className="pb-element pb-text-element"
+                        style={{'backgroundColor': `rgba(${r},${g},${b},${a})`}}
+                    >
+                        <TinyMCE
+                            content="<p>This is the initial content of the editor</p>"
+                            config={{
+                                inline: true,
+                                menubar: false,
+                                toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | fontsizeselect fontselect',
+                                fontsize_formats: '8pt 10pt 12pt 14pt 18pt 24pt 36pt',
+                                font_formats: 'Arial=arial,helvetica,sans-serif;Courier New=courier new,courier,monospace;AkrutiKndPadmini=Akpdmi-n'
+                            }}
+                            onChange={handleEditorChange}
+                        />
+                    </div>);
+                break;
+            case 'button':
+                element = (
+                    <div
+                        className="pb-element pb-button-element"
+                        style={{'backgroundColor': `rgba(${r},${g},${b},${a})`}}
+                    >
+                        <a href="#" className="btn btn-primary">
+                            Sample button text
+                            {/* e.preventDefault() */}
+                        </a>
+                    </div>);
+                break;
+            case 'heading':
+                element = (
+                    <div
+                        className="pb-element pb-heading-element"
+                        style={{'backgroundColor': `rgba(${r},${g},${b},${a})`}}
+                    >
+                        <TinyMCE
+                            content="<h2>This is simple heading</h2>"
+                            config={{
+                                inline: true,
+                                linkchecker_api_key: 'idds1ja21nirk6ca3rxkpxi0mirk22w2zyvzz1f3y15dzelg',
+                                plugins: 'link image code',
+                                toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code'
+                            }}
+                            onChange={handleEditorChange}
+                        />
+                    </div>);
+                break;
+            case 'link':
+                element = (
+                    <div
+                        className="pb-element pb-link-element"
+                        style={{'backgroundColor': `rgba(${r},${g},${b},${a})`}}
+                    >
+                        <a href="#">
+                            Sample link
+                            {/* e.preventDefault() */}
+                        </a>
+                    </div>);
+                break;
 
-						default:
-								break;
-				}
-		}
-		//const {r, g, b, a} = [255, 255, 255, 0];
+            default:
+                break;
+        }
+    }
+    //const {r, g, b, a} = [255, 255, 255, 0];
 
 
-		return (
-				<div className=''
-						 onClick={handleClickContextMenu}
-						 onContextMenu={handleClickContextMenu}
-				>
-						{element}
-				</div>
-		);
+    return (
+        <div className=''
+             onClick={handleClickContextMenu}
+             onContextMenu={handleClickContextMenu}
+        >
+            {element}
+        </div>
+    );
 }
 
 
